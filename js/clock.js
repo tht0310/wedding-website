92381deb5d8c42eef7e5cf85142799487e7c9ef4
Trần Hoàng Tiến <thtien0310@gmail.com>
2025-11-17 09:59:32 +0700

    temp commit

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
   let clock;
 
   // Grab the current date
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2023-10-29 12:00", "Asia/Kolkata");
+  let targetDate = moment.tz("2026-01-17 11:00", "Asia/Ho_Chi_Minh");
 
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
@@ -15,33 +15,32 @@ $(document).ready(function() {
     clock = $(".clock").FlipClock(0, {
       clockFace: "DailyCounter",
       countdown: true,
-      autostart: false
+      autostart: false,
     });
-    console.log("Date has already passed!")
-    
+    console.log("Date has already passed!");
   } else {
     // Run countdown timer
     clock = $(".clock").FlipClock(diff, {
       clockFace: "DailyCounter",
       countdown: true,
       callbacks: {
-        stop: function() {
-          console.log("Timer has ended!")
-        }
-      }
+        stop: function () {
+          console.log("Timer has ended!");
+        },
+      },
     });
-    
+
     // Check when timer reaches 0, then stop at 0
-    setTimeout(function() {
+    setTimeout(function () {
       checktime();
     }, 1000);
-    
+
     function checktime() {
       t = clock.getTime();
       if (t <= 0) {
         clock.setTime(0);
       }
-      setTimeout(function() {
+      setTimeout(function () {
         checktime();
       }, 1000);
     }
